309ad2c95d90091a90f30135d219221f0e2bb600
Shannan Oneill <s.oneill@thewosgroup.com>
2022-03-18 12:30:34 +0000

    Put tut1 puzzle in its own file

--- Program.cs
+++ Program.cs
@@ -1,109 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Puzzle Version One, Tutorial");
-
-Console.WriteLine("You are at the start of a puzzle. You can go up or you can go left. When you start moving in a direction, you don't stop until you hit a wall");
-Console.WriteLine("Your goal is to reach the exit");
-
-int x = 0;
-int y = 0;
-String direction = null;
-String UpperDirection = null;
+﻿using System;
 
 
-
-while (UpperDirection != "U" && UpperDirection != "L")
+namespace Puzzle1
 {
-    Console.WriteLine("You can go up or you can go left");
-    Console.WriteLine("U = Up, D = Down, L = Left, R = Right");
-    direction = Console.ReadLine();
-    UpperDirection = direction.ToUpper();
-
-    if (direction != null)
+    class Program
     {
-        if (UpperDirection == "U")
+        static void Main(string[] args)
         {
-            x = 0;
-            y = 3;
-        }
-        else if (UpperDirection == "L")
-        {
-            x = 4;
-            y = 0;
-        }
-
-    }
-    else
-    {
-        Console.WriteLine("Try again....");
-    }
-}
-
-Console.WriteLine("You hit a wall!");
-
-if (x == 0 && y == 3)
-{
-    Console.WriteLine("You can go left or you can go down");
-    direction = Console.ReadLine();
-    UpperDirection = direction.ToUpper();
-
-    if (UpperDirection == "L" || UpperDirection == "D")
-    {
-        if (UpperDirection == "L")
-        {
-            x = 4;
-            y = 3;
-        }
-        else if (UpperDirection == "D")
-        {
-            x = 0;
-            y = 0;
-        }
-        else
-        {
-            Console.WriteLine("Try again...jk haven't finished this part yet");
+            TutorialPuzzle1 puzzle1 = new TutorialPuzzle1();
+            puzzle1.TutorialPuzzleMethod();
         }
     }
 }
 
-else if (x == 4 && y == 0)
-{
-    Console.WriteLine("You can go up or you can go left");
-    direction = Console.ReadLine();
-    UpperDirection = direction.ToUpper();
 
-    if (UpperDirection == "U" || UpperDirection == "L")
-    {
-        if (UpperDirection == "U")
-        {
-            x = 4;
-            y = 3;
-        }
-        else if (UpperDirection == "L")
-        {
-            x = 0;
-            y = 0;
-        }
-        else
-        {
-            Console.WriteLine("Try again...jk haven't finished this part yet");
-        }
-    }
-}
-else
-{
-    Console.WriteLine("idk something went wrong...");
-}
 
-if (x == 4 && y == 3)
-{
-    Console.WriteLine("Yay you win!!! You reached the end!!!");
-}
-else
-{
-    Console.WriteLine("Lol I need to change it so that you can keep moving until you get to the end");
-}
-
-Console.WriteLine("End of Tutorial Puzzle 1");
 
 
 // Make it so you can keep moving until you reach the end
@@ -111,7 +22,7 @@ Console.WriteLine("End of Tutorial Puzzle 1");
 // Add info about the size of the grid
 // Add where you need to reach to win on the grid
 // Add info on where you are on the grid after each turn
-// Put this in its own class file
+// Put tut1 puzzle in its own file ////DONE////
 
 
 
---
+++ TutorialPuzzle1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle1
+{
+    class TutorialPuzzle1
+    {
+        public void TutorialPuzzleMethod()
+        {
+            Console.WriteLine("Puzzle Version One, Tutorial");
+
+            Console.WriteLine("You are at the start of a puzzle. You can go up or you can go left. When you start moving in a direction, you don't stop until you hit a wall");
+            Console.WriteLine("Your goal is to reach the exit");
+
+            int x = 0;
+            int y = 0;
+            String direction = null;
+            String UpperDirection = null;
+
+
+
+            while (UpperDirection != "U" && UpperDirection != "L")
+            {
+                Console.WriteLine("You can go up or you can go left");
+                Console.WriteLine("U = Up, D = Down, L = Left, R = Right");
+                direction = Console.ReadLine();
+                UpperDirection = direction.ToUpper();
+
+                if (direction != null)
+                {
+                    if (UpperDirection == "U")
+                    {
+                        x = 0;
+                        y = 3;
+                    }
+                    else if (UpperDirection == "L")
+                    {
+                        x = 4;
+                        y = 0;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Try again....");
+                }
+            }
+
+            Console.WriteLine("You hit a wall!");
+
+            if (x == 0 && y == 3)
+            {
+                Console.WriteLine("You can go left or you can go down");
+                direction = Console.ReadLine();
+                UpperDirection = direction.ToUpper();
+
+                if (UpperDirection == "L" || UpperDirection == "D")
+                {
+                    if (UpperDirection == "L")
+                    {
+                        x = 4;
+                        y = 3;
+                    }
+                    else if (UpperDirection == "D")
+                    {
+                        x = 0;
+                        y = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again...jk haven't finished this part yet");
+                    }
+                }
+            }
+
+            else if (x == 4 && y == 0)
+            {
+                Console.WriteLine("You can go up or you can go left");
+                direction = Console.ReadLine();
+                UpperDirection = direction.ToUpper();
+
+                if (UpperDirection == "U" || UpperDirection == "L")
+                {
+                    if (UpperDirection == "U")
+                    {
+                        x = 4;
+                        y = 3;
+                    }
+                    else if (UpperDirection == "L")
+                    {
+                        x = 0;
+                        y = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again...jk haven't finished this part yet");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("idk something went wrong...");
+            }
+
+            if (x == 4 && y == 3)
+            {
+                Console.WriteLine("Yay you win!!! You reached the end!!!");
+            }
+            else
+            {
+                Console.WriteLine("Lol I need to change it so that you can keep moving until you get to the end");
+            }
+
+            Console.WriteLine("End of Tutorial Puzzle 1");
+        }
+    }
+}
